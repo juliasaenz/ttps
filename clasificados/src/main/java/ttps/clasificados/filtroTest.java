acbfcb405ffc765fb315bc03fa15ed529cae51e6
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-09-12 08:26:38 -0300

    initial commit

---
+++ clasificados/src/main/java/ttps/clasificados/filtroTest.java
@@ -0,0 +1,20 @@
+package ttps.clasificados;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class filtroTest implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
