1a25547d9746bb79b26c68f820210e28ae451806
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-11-14 10:05:56 -0300

    agregado ejemplo de catedra de srping

---
+++ spring_core_2024/ttps-spring/src/main/java/ttps/spring/test/SpringTestMain.java
@@ -0,0 +1,35 @@
+package ttps.spring.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ttps.spring.model.MiPrimerEntity;
+import ttps.spring.config.PersistenceConfig;
+import javax.sql.DataSource;
+
+
+public class SpringTestMain {
+	public static void main(String[] args) {
+		 // Create a new AnnotationConfigApplicationContext
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        
+        // Register the configuration class (PersistenceConfig in this case)
+        ctx.register(PersistenceConfig.class);
+
+        // Refresh the context to initialize Spring beans
+        ctx.refresh();
+
+     	
+		DataSource dataSource = ctx.getBean(DataSource.class);
+        System.out.println("DataSource cargado: " + dataSource);
+        
+        
+        MiPrimerEntity e = ctx.getBean("prueba", MiPrimerEntity.class);
+		System.out.println("Bean cargado: "+e.hola());
+
+        // Don't forget to close the context to free main.resources
+        ctx.close();
+		
+
+	
+	}
+
+}
