c982b36247ce8d8ec25024548268dfe87027b731
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-11-20 16:05:02 -0300

    agregado boot para correr la app

---
+++ entregable5/src/main/java/ttps/spring/test/BootTest.java
@@ -0,0 +1,13 @@
+package ttps.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "ttps.spring")
+public class BootTest {
+    public static void main(String[] args) {
+        SpringApplication.run(BootTest.class, args);
+    }
+}
