3713ed56a58055126604933c0b679e8fb0e39181
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-12-16 15:34:30 -0300

    cambie de lugar unos archivos

--- que-comemos/src/app/app.routes.ts
+++ que-comemos/src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ClienteComponent } from './components/registro/cliente/cliente.component';
+import { AdminComponent } from './components/registro/admin/admin.component';
+import { ResponsableComponent } from './components/registro/responsable/responsable.component';
+
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'registro/cliente', component: ClienteComponent },
+  { path: 'registro/responsable', component: ResponsableComponent },
+  { path: 'registro/admin', component: AdminComponent },
 ];
---
+++ que-comemos/src/app/components/registro/responsable/responsable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsable',
+  imports: [],
+  templateUrl: './responsable.component.html',
+  styleUrl: './responsable.component.css'
+})
+export class ResponsableComponent {
+
+}
