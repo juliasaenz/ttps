365c85e23703687e5b4c80a7f01d1ce54285ed97
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-12-17 09:53:45 -0300

    boton registro funciona

--- que-comemos/src/app/components/home/home.component.ts
+++ que-comemos/src/app/components/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  goToRegistro() {
+    this.router.navigate(['/registro']);
+  }
 }
