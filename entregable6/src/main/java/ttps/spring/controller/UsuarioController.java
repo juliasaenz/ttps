7a917e5ea9aa5596f78ba3f36e2269b5f87a1311
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-12-20 10:57:27 -0300

    integrado registro y login (cliente) funcional

--- entregable6/src/main/java/ttps/spring/controller/UsuarioController.java
+++ entregable6/src/main/java/ttps/spring/controller/UsuarioController.java
@@ -4,7 +4,6 @@ import java.util.List; // Asegúrate de importar List
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ttps.spring.model.UsuarioLogin; // Asegúrate de importar UsuarioLogin
 import ttps.spring.model.Usuario;
 import ttps.spring.service.UsuarioService;
 import java.util.Map;
--- que-comemos/src/app/components/registro/exito/exito.component.ts
+++ que-comemos/src/app/components/registro/exito/exito.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-exito',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './exito.component.html',
   styleUrl: './exito.component.css'
 })
