ee7813080be149d999dcbc3c86529ccd02b109c9
Julia Saenz Waslet <julisaenz99@gmail.com>
2024-12-20 09:34:06 -0300

    carta as class

--- que-comemos/src/app/components/carta/carta.component.ts
+++ que-comemos/src/app/components/carta/carta.component.ts
@@ -18,8 +18,8 @@ export class CartaComponent implements OnInit {
   parsedMenus: { id: number; nombre: string }[] = [];
   parsedMenusVeggie: { id: number; nombre: string }[] = [];
 
-  newCarta: Carta = this.initializeCarta();
-  editableCarta: Carta = this.initializeCarta();
+  newCarta: Carta = new Carta();
+  editableCarta: Carta = new Carta();
   isEditPopupVisible = false;
 
   constructor(
@@ -55,7 +55,7 @@ export class CartaComponent implements OnInit {
   }
 
   resetNewCarta(): void {
-    this.newCarta = this.initializeCarta();
+    this.newCarta = new Carta();
   }
 
   loadMenus(): void {
@@ -81,7 +81,7 @@ export class CartaComponent implements OnInit {
 
   closeEditPopup(): void {
     this.isEditPopupVisible = false;
-    this.editableCarta = this.initializeCarta();
+    this.editableCarta = new Carta();
   }
 
   confirmEdit(): void {
@@ -103,14 +103,6 @@ export class CartaComponent implements OnInit {
     } else {
       alert('Todos los campos son obligatorios.');
     }
-  }
-
-  private initializeCarta(): Carta {
-    return {
-      dia: new Date(),
-      menu: null,
-      menuVeggie: null,
-    };
   }
 
   private formatDate(date: Date): string {
